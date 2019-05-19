332bc83a877af0f1d52dde908e916c0636f1e16c
Eumji <jihye97@ajou.ac.kr>
2019-05-20 04:14:14 +0900

    플랭크 인식 추가 , 점핑잭 스텝 늘리고 인식 방식 바꿈

--- ParTner/app/src/main/java/com/example/partner/Flank.java
+++ ParTner/app/src/main/java/com/example/partner/Flank.java
@@ -1,5 +1,7 @@
 package com.example.partner;
 
+import android.util.Log;
+
 public class Flank extends Exercise{
 
     private float[][] point;
@@ -11,15 +13,47 @@ public class Flank extends Exercise{
     @Override
     public boolean checkReady(){
         boolean isReady = false;
+
+        //head-neck-shoulder-hip은 180도도
+        //hip-knee-ankle은 90도
+        //shoulder-elbow-wrist 90도
+        double hnsAngle = getAngle(1,0,1,2);
+        double nshAngle = getAngle(2,1,2,8);
+        double hkaAngle = getAngle(9,8,9,10);
+        double sewAngle = getAngle(3,2,3,4);
+
+        if(hnsAngle>=110 && hnsAngle <=180 &&
+            nshAngle>=110 && nshAngle<= 180)
+//            && hkaAngle>=40 && hkaAngle<=100 &&
+//            sewAngle>=40 && hkaAngle<=100)
+            isReady=true;
+        Log.d("jumpingjack", hnsAngle+","+nshAngle+","+hkaAngle+","+sewAngle);
+
         return isReady;
     }
 
     // 플랭크 운동 동작 인식하는 함수
     @Override
-    public boolean doExercise(int currentStep){
+    public boolean doExercise(int currentStep) {
+        //head-neck-shoulder-hip-knee-ankle은 180
+        //shoulder-elbow-wrist 90도
+        double hnsAngle = getAngle(1, 0, 1, 2);
+        double nshAngle = getAngle(2, 1, 2, 8);
+        double shkAngle = getAngle(8, 2, 8, 9);
+        double hkaAngle = getAngle(9, 8, 9, 10);
+        double sewAngle = getAngle(3, 2, 3, 4);
+        Log.d("jumpingjack", hnsAngle+","+nshAngle+","+shkAngle+","+hkaAngle+","+sewAngle);
 
+
+        if (hnsAngle >= 110 && hnsAngle <= 180 &&
+                nshAngle >= 110 && nshAngle <= 180 &&
+                shkAngle >= 110 && shkAngle <= 180 &&
+                hkaAngle >= 110 && hkaAngle <= 180)
+//                && sewAngle >= 40 && hkaAngle <= 100)
+            return true;
         return false;
     }
+
 
     // getter setter
     public float[][] getPoint() {
@@ -28,5 +62,10 @@ public class Flank extends Exercise{
 
     public void setPoint(float[][] point) {
         this.point = point;
+    }
+
+    @Override
+    public int getSteps() {
+        return 20;
     }
 }
